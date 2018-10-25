07e87812ae5dcda9a46a0b32e534b45662ab7acc
Sergey Averyanov <railtrailtime@gmail.com>
2018-10-25 14:37:08 +0300

    Remove debug bot token

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    token: '604425385:AAGyGEXZiBsqR7nOVXtszj0MdMuzSlw53qE',    // you can get it by writing @BotFather in telegram
+    token: 'YOUR_TELEGRAM_BOT_TOKEN',    // you can get it by writing @BotFather in telegram
     storage: path.join(__dirname, '/storage'),
     adminChatId: 446820726
 }
